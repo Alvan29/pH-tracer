db88810c8225ac26199126790b40bafbb1226c84
Alvan29 <avinalvan29@gmail.com>
2025-03-23 16:40:28 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,php,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins'],
+        DmSans: ['DM Sans', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
